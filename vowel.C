1e961eaa2d938f2315d89a9d12c7a5feeb82460d
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-10 15:54:43 +0530

    vowel.c

--- vowel.C
+++ vowel.C
@@ -2,12 +2,12 @@
 int main()
 {
 char c;
-int upr,lwr;
+int u,l;
 printf("Enter an alphabet: ");
 scanf("%c",&c);
 lwr=(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 upr=(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-if (lwr||upr)
+if (l||u)
 printf("%c is a vowel.", c);
 else
 printf("%c is a consonant.", c);
