5e18b7d781545697059c53affb161cf4750497af
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-10 16:26:36 +0530

    palin.C

--- palin.C
+++ palin.C
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-int n, reversedInteger = 0, rem;
+int n, rev = 0, r;
 printf("Enter an integer: ");
 scanf("%d", &n);
 while( n!=0 )
 {
 rem= n%10;
-reversedInteger = reversedInteger*10 + rem;
+rev = rev*10 + r;
 n /= 10;
 }
-if (n== reversedInteger)
+if (n== rev)
 printf("%d is a palindrome.",n);
 else
 printf("%d is not a palindrome.",n);
