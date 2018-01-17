dc48244bb4b0f6510639f6fff6aff9c0a1cab234
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-17 16:03:12 +0530

    fact.C

--- fact.C
+++ fact.C
@@ -4,7 +4,7 @@ int main()
   int n,f=1,i;
   printf("enter the value of n:");
   scanf("%d",&n);
-  for(i=0;i<=n;i++)
+  for(i=1;i<=n;i++)
   f=f*i;
   printf("the factorial of %d:%d",n,f);
   return 0;
