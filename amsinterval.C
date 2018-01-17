2aa608f92af6a9b1b789dd3b403e3d85043e55b3
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-17 16:17:42 +0530

    amsinterval.C

--- amsinterval.C
+++ amsinterval.C
@@ -1,3 +1,31 @@
-#include<stdio.h>
-int main()
-{
+  #include <stdio.h>
+  #include <math.h>
+  int main()
+  {
+  int a,b, i, t1, t2, rem, n = 0, r= 0;
+  printf("Enter two numbers(intervals): ");
+  scanf("%d %d", &a, &b);
+  printf("Armstrong numbers between %d an %d are: ", a,b);
+  for(i = a+ 1; i <b; ++i)
+  {
+  t2 = i;
+  t1 = i;
+  while (t1 != 0)
+  {
+  t1/= 10;
+  ++n;
+  }
+  while (t2 != 0)
+  {
+  rem= t2% 10;
+  r+= pow(rem, n);
+   t2/= 10;
+   }
+   if (r== i) {
+   printf("%d ", i);
+   }
+   n = 0;
+   r= 0;
+   }
+   return 0;
+   }
