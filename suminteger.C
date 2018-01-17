47e7ee8d5493a8037d651f7c8d39e4d16b2d21e4
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-17 16:59:07 +0530

    suminteger.C

--- suminteger.C
+++ suminteger.C
@@ -1,10 +1,19 @@
-#include<stdio.h>
-int main()
-{
-int a[100],i,k,n;
-printf("enter the total no.of elements:");
-scanf("%d",&n);
-printf("enter the values of array:");
-for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-  
+    int main()
+    {
+    int a[100],i,n,k,s=0;
+    printf("Enter size of the array:");
+    scanf("%d", &n);
+    printf("Enter %d elements in the array:",n);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    printf("enter the no.of values to be added:");
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+    s=s+a[i];
+    }
+    printf("Sum of all elements of array = %d",s);
+    return 0;
+    }
