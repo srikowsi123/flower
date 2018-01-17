0e683f2161fafe7199ceedc01dd500b103d5f052
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-17 16:00:38 +0530

    fact.C

--- fact.C
+++ fact.C
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  int n,f=1,i;
+  printf("enter the value of n:");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+  f=f*i;
+  printf("the factorial of %d:%d",n,f);
+  return 0;
+}
