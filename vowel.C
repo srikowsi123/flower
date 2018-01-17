6cbde9534b87fd08c75b587bdabf4e74a8a89085
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-17 16:48:41 +0530

    Update vowel.C

--- vowel.C
+++ vowel.C
@@ -5,8 +5,8 @@ char c;
 int u,l;
 printf("Enter an alphabet: ");
 scanf("%c",&c);
-lwr=(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-upr=(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+l=(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+u=(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 if (l||u)
 printf("%c is a vowel.", c);
 else
