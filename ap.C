7da8b586b8629d4cc55701025b9ca1a4d420d9c1
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-01-22 16:59:18 +0530

    ap.C

--- ap.C
+++ ap.C
@@ -2,18 +2,17 @@
 int main()
 {
       int n,i;
-      int sum=0;
-      printf("1+2+3+...+N");
+      int s=0;
       printf("\n\nEnter Value of N : ");
       scanf("%d",&n);
-      sum = (n * (n + 1)) / 2;  // Mathematical formula for sum of series.
-      for(i =1;i <= n;i++)
+      s=(n*(n + 1))/2;
+      for(i =1;i<=n;i++)
       {
-            if (i!=n)
-                  printf("%d + ",i);
-            else
-                  printf("%d ",i);
+        if (i!=n)
+        printf("%d + ",i);
+        else
+        printf("%d ",i);
       }
-      printf("\n\nSum of Series = %d",sum);
+      printf("\n\nSum of Series = %d",s);
       return 0;
 }
