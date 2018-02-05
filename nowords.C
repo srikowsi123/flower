63f975fabe0af7a8e9083007fd14a11e197bfbb7
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-05 16:17:44 +0530

    nowords.C

---
+++ nowords.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    char a[20];
+    int i,words;
+    printf("enter the string");
+    for(i=0;i<n)
+    words=1;
+    while(a[i]!='\n')
+    {
+    	if(a[i]==' '||a[i]=='\n'||a[i]=='\t')
+        {
+            words++;
+        }
+
+        i++;
+    }
+	printf("total no.of words: %d",words);
+	return 0;
+}
