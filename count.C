5d4fd39f2a4f467edfc5d0d63aba7480730b5b89
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-07 16:41:37 +0530

    count.C

--- count.C
+++ count.C
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main()
 {
-long long n;
+int n;
 int count = 0;
 printf("Enter an integer: ");
-scanf("%lld", &n);
+scanf("%d", &n);
 while(n != 0)
 {
 n = n/10;
 ++count;
 }
 printf("Number of digits: %d", count);
+return 0;
 }
