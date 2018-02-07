72a9454c30eb40b32fe6e4e19e9ce092c70ad617
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-07 16:56:02 +0530

    47.C

---
+++ 47.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,n,max,min;
+	printf("enter the limit:");
+	scanf("%d",&n);
+	printf("the values are:");
+	for(i=0;i<n;i++)
+	scanf("%d \n",&a[i]);
+	max=a[0];
+    for(i =1;i<n;i++)
+    {
+      if (max<a[i])
+      max=a[i];
+    }
+    printf(" the largest value is: %d",max);
+    min=a[0];
+    for(i =1;i<n;i++)
+    {
+     if(min>a[i])
+     min=a[i];	
+    }
+    printf(" the smallest value is: %d",min);
+    return 0;
+}
