8663ef5410be5bf235091803447f32ed051e05be
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-07 16:34:04 +0530

    41.C

--- 41.C
+++ 41.C
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	int n;
+	printf("enter the string:");
+	gets(s);
+	printf("enter the number:");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	printf("%s",s);
+	return 0;
+}
