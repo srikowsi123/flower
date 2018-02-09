cd819b872575b18dd32e35790bba5c38ba880548
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-09 15:49:22 +0530

    48.C

--- 48.C
+++ 48.C
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main()
+{
+int n, sum = 0,i, value,avg;
+printf("Enter the number of integers you want to add:");
+scanf("%d", &n);
+printf("Enter %d integers:",n);
+for (i=1;i<= n;i++)
+{
+scanf("%d", &value);
+sum = sum + value;
+}
+avg=sum/n;
+printf("average is: %d",avg);
+return 0;
+}
