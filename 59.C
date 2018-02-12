92bdc73e75f870099b24a87c948d507ac757d6b1
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-12 16:52:31 +0530

    59.C

--- 59.C
+++ 59.C
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int a[100],i,n=10,max;
+	int n[100],i,b=10,max;
 	printf("the values are:");
-	for(i=0;i<n;i++)
-	scanf("%d \n",&a[i]);
-	max=a[0];
-    for(i =1;i<n;i++)
+	for(i=0;i<b;i++)
+	scanf("%d \n",&n[i]);
+	max=n[0];
+    for(i =1;i<b;i++)
     {
-      if (max<a[i])
-      max=a[i];
+      if (max<n[i])
+      max=n[i];
     }
     printf(" the largest value is: %d",max);
     return 0;
