7921fdf6ff48846cfa206f1f4e5e6daf31c9fc09
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-12 16:55:42 +0530

    60.C

--- 60.C
+++ 60.C
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-int i,n,a=0,b=1,c;
+int i,n,p=0,q=1,r;
 printf("Enter the number of terms: ");
 scanf("%d",&n);
 printf("Fibonacci Series: ");
 for(i=0;i<n;++i)
 {
-printf("%d \n",b);
-c=a+b;
-a=b;
-b=c;
+printf("%d \n",q);
+r=p+q;
+p=q;
+q=r;
 }
 return 0;
 }
