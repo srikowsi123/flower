e22e19397a97be538d6331beccfe08c837a66647
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-02-12 16:50:10 +0530

    58.C

--- 58.C
+++ 58.C
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() 
 {
-    int i,k;
+    int a,b;
     printf("enter the value:");
-    scanf("%d %d",&i,&k);
-    printf("before swapping %d %d", i, k);
-    i = i ^ k;
-    k = i ^ k;
-    i = i ^ k;
-    printf("after swapping %d %d", i, k);
+    scanf("%d %d",&a,&b);
+    printf("before swapping %d %d", a,b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    printf("after swapping %d %d",a,b);
     return 0;
 }
