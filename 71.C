5c6d06ea10b63ee37d694cf335865a2cadd8ecf7
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-05 16:28:00 +0530

    71.C

---
+++ 71.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1[100],s2[100];
+int p;
+printf("enter the input \n");
+scanf("%s",s1);
+strcpy(s2,s1);
+strrev(s1);
+p=strcmp(s1,s2);
+if(p==0)
+{
+printf("palindrome \n");
+}
+else
+{
+printf("not a palindrome \n");
+}
+return 0;
+}
