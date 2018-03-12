3e7e20fbad5f44eb0e85d2a9b16ae0c0092814dd
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-12 16:37:04 +0530

    94.C

---
+++ 94.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+	int i,n,k,a[20];
+	scanf("%d %d",&n,&k);
+	for(i=1;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=1;i<=n;i++)
+	{
+		while(i==k)
+		{
+			printf("\n%d",a[i]);
+			break;
+		}
+	}
+	return 0;
+}
