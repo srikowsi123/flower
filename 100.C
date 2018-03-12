85b4e6794d1db35a1d89a13717310c96f8426196
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-12 15:44:24 +0530

    100.C

---
+++ 100.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n, s=1,i, value;
+printf("Enter the number of integers:");
+scanf("%d", &n);
+printf("Enter %d integers:",n);
+for (i=1;i<= n;i++)
+{
+scanf("%d", &value);
+s = s + value;
+}
+printf("multiplication of entered integers:%d",s);
+return 0;
+}
