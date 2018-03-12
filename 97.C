8dbedd156ca81da574f86b9e3e259c4a908a3ec1
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-12 16:11:39 +0530

    97.C

---
+++ 97.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+ {
+int n,rem,rev=0;
+ int res;
+ printf("\n enter the number:");
+ scanf("%d",&n);
+ while(n!=0)
+ {
+ rem=n%10;
+ rev=rev*10+rem;
+ n=n/10;
+ }
+ printf("\n%d",rev);
+return 0;
+ }
