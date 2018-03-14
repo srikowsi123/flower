47591fb8eca241cc328838a2168dfde08eea5984
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-14 16:00:59 +0530

    95.C

--- 95.C
+++ 95.C
@@ -1,10 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int p,t,r,si;
+int p,t,r,s;
+float si;
 printf("enter the value:");
 scanf("%d %d %d",&p,&t,&r);
-si=(p*t*r)/100;
-printf("the simple interest: %d",si);
+s=(p*t*r);
+si=(s/100);
+printf("the simple interest: %f",si);
 return 0;
 }
