b44dbb979d1f52eb51d8185def48ee2e78a573c1
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-16 16:53:53 +0530

    101.C

--- 101.C
+++ 101.C
@@ -3,15 +3,15 @@
 int main()
 { 
 char a[100];
-int n,l,i,count=0;
+int n,l,i,c=0;
 scanf("%s",a);
 scanf("%d",&l);
 n=strlen(a);
 for(i=n-1;i>=0;i--)
 {
 printf("%c",a[i]);
-count++;
-if(l==count)
+c++;
+if(l==c)
 {
 break;
 }
