66aa16d56e9383a6f817a9ca1fdba4b9d4565e43
srikowsi123 <30549758+srikowsi123@users.noreply.github.com>
2018-03-19 15:59:00 +0530

    112.C

---
+++ 112.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+int i,n,k,a[50],t;
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+t=1;
+break;
+}
+}
+if(t==1)
+{
+printf("\nyes");
+}	
+else
+{
+printf("\nno");
+}
+return 0;
+}
